47032891a8e74529ee0e2fc615555d0b50445f91
lar-mo <lmoiola@gmail.com>
2021-03-18 08:59:59 -0700

    branch work for displaying privacy policy

---
+++ js/overlay.js
@@ -0,0 +1,23 @@
+/* Open */
+function openpp() {
+  /*from left*/
+  document.getElementById("privacy").style.width = "100%";
+  /*from top*/
+  // document.getElementById("myNav").style.height = "100%";
+  /*no anim*/
+  // document.getElementById("myNav").style.display = "block";
+  // $('#privacy').css('display','block');
+  // $('#myNav').css('display','block');
+}
+
+/* Close */
+function closepp() {
+  /*from left*/
+  document.getElementById("privacy").style.width = "0%";
+  /*from top*/
+  // document.getElementById("myNav").style.height = "0%";
+  /*no anim*/
+  // document.getElementById("myNav").style.display = "none";
+  // $('#privacy').css('display','none');
+  // $('#myNav').css('display','none');
+}
