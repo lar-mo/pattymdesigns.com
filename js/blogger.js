dc0d5af52b484043e43b7918eb79e04dc05ca13e
lar-mo <lmoiola@gmail.com>
2021-03-28 02:04:10 -0700

    moving wrapped api calls; code cleanup

--- js/blogger.js
+++ js/blogger.js
@@ -1,33 +1,13 @@
-let bloggerv3_url = 'https://www.googleapis.com/blogger/v3/blogs/6624714559657687469/posts?'
+let bloggerv3_url = 'https://api.pattymdesigns.com/wrapper/bloggerApiGetLatestPost/'
   function getLatestPost() {
-    // 5929721860344397604 - https://roadtripsandhikes.blogspot.com
-    // 6624714559657687469 - https://pattymdesigns.blogspot.com
-    let config = {
-      params: {
-        key: blogger_apiv3,
-        fetchBodies: true,
-        fetchImages: true,
-        maxResults: 1,
-        orderBy: 'PUBLISHED',
-      }, // end of 'params'
-    } // end of 'config'
-    axios.get(bloggerv3_url, config)
-    .then(function (response) {                                     // then do the cool stuff
-      let data = response.data.items[0];                                      // get data from api response
+    axios.get(bloggerv3_url)
+    .then(function (response) {
+      let data = response.data.items[0];
       let image_url = data.images[0].url;
       let post_body = data.content;
       let post_title = data.title;
       let post_published = JSON.stringify(data.published).slice(1,11);
       let post_url = data.url;
-      // console.log(data);
-      // console.log(data.url);
-      // console.log(data.title);
-      // console.log(post_published);
-      // console.log(data.images[0].url);
-      // console.log(response.status);
-      // console.log(response.statusText);
-      // console.log(response.headers);
-      // console.log(response.config);
       var date_pub = new Date(post_published);
       var month = date_pub.getMonth();
       var months = {
@@ -71,7 +51,6 @@ let bloggerv3_url = 'https://www.googleapis.com/blogger/v3/blogs/662471455965768
       read_more.setAttribute('class', "read_more");
       read_more.innerHTML += "<a href='" + post_url + "' target='_blank'>Read More</a>";
       pub_date_read_more_div.appendChild(read_more);
-      // desc_div.innerHTML += "<div>" + post_body + "</div>";
 
       desc_div.appendChild(pub_date_read_more_div);
 
