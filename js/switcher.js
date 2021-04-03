2f300b76599380d66c441cf8c49cfb1a3e928a1d
lar-mo <lmoiola@gmail.com>
2021-04-02 20:59:21 -0700

    change label to word instead of abbr.

--- js/switcher.js
+++ js/switcher.js
@@ -3,21 +3,21 @@ let url_arg = window.location.search;
 $(document).ready(function() {
   if (url_arg == '?es' || lang == "es") {
     $('.center-text a').attr("href","privacy.html?es");
-    $(lang_label).text("ES");
+    $(lang_label).text("Español");
     $("#es_option").css("display","none");
   }
   else if (url_arg == '?fr' || lang == "fr") {
     $('.center-text a').attr("href","privacy.html?fr");
-    $(lang_label).text("FR");
+    $(lang_label).text("Français");
     $("#fr_option").css("display","none");
   }
   else if (url_arg == '?de' || lang == "de") {
     $('.center-text a').attr("href","privacy.html?de");
-    $(lang_label).text("DE");
+    $(lang_label).text("Deutsche");
     $("#de_option").css("display","none");
   }
   else {
-    $(lang_label).text("EN");
+    $(lang_label).text("English");
     $("#en_option").css("display","none");
   }
 });
@@ -27,7 +27,7 @@ $('.dropdown-content a:nth-of-type(1)').click(function(){
   lang = 'en';
   $('.center-text a').attr("href","privacy.html");
   window.history.replaceState(null, null, "?en");
-  $(lang_label).text('EN');
+  $(lang_label).text('English');
   $("#en_option").css("display","none");
   $("#es_option").css("display","block");
   $("#fr_option").css("display","block");
@@ -39,7 +39,7 @@ $('.dropdown-content a:nth-of-type(2)').click(function(){
   lang = 'es';
   $('.center-text a').attr("href","privacy.html?es");
   window.history.replaceState(null, null, "?es");
-  $(lang_label).text('ES');
+  $(lang_label).text('Español');
   $("#en_option").css("display","block");
   $("#es_option").css("display","none");
   $("#fr_option").css("display","block");
@@ -51,7 +51,7 @@ $('.dropdown-content a:nth-of-type(3)').click(function(){
   lang = 'fr';
   $('.center-text a').attr("href","privacy.html?fr");
   window.history.replaceState(null, null, "?fr");
-  $(lang_label).text('FR');
+  $(lang_label).text('Français');
   $("#en_option").css("display","block");
   $("#es_option").css("display","block");
   $("#fr_option").css("display","none");
@@ -63,7 +63,7 @@ $('.dropdown-content a:nth-of-type(4)').click(function(){
   lang = 'de';
   $('.center-text a').attr("href","privacy.html?de");
   window.history.replaceState(null, null, "?de");
-  $(lang_label).text('DE');
+  $(lang_label).text('Deutsche');
   $("#en_option").css("display","block");
   $("#es_option").css("display","block");
   $("#fr_option").css("display","block");
