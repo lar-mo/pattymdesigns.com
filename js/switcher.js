a5d3db7e41827a2b73613f07a74622cf6a526dc6
lar-mo <lmoiola@gmail.com>
2021-04-03 19:53:15 -0700

    removing lang var

--- js/switcher.js
+++ js/switcher.js
@@ -1,17 +1,17 @@
 let url_arg = window.location.search;
 
 $(document).ready(function() {
-  if (url_arg == '?es' || lang == "es") {
+  if (url_arg == '?es') {
     $('.center-text a').attr("href","privacy.html?es");
     $(lang_label).text("Español");
     $("#es_option").css("display","none");
   }
-  else if (url_arg == '?fr' || lang == "fr") {
+  else if (url_arg == '?fr') {
     $('.center-text a').attr("href","privacy.html?fr");
     $(lang_label).text("Français");
     $("#fr_option").css("display","none");
   }
-  else if (url_arg == '?de' || lang == "de") {
+  else if (url_arg == '?de') {
     $('.center-text a').attr("href","privacy.html?de");
     $(lang_label).text("Deutsche");
     $("#de_option").css("display","none");
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
 let local_storage_strings = localStorage.getItem('all_pmd_strings');
 $('.dropdown-content a:nth-of-type(1)').click(function(){
-  lang = 'en';
+  // lang = 'en';
   $('.center-text a').attr("href","privacy.html");
   window.history.replaceState(null, null, "?en");
   $(lang_label).text('English');
@@ -36,7 +36,7 @@ $('.dropdown-content a:nth-of-type(1)').click(function(){
   loadFormbutton();
 })
 $('.dropdown-content a:nth-of-type(2)').click(function(){
-  lang = 'es';
+  // lang = 'es';
   $('.center-text a').attr("href","privacy.html?es");
   window.history.replaceState(null, null, "?es");
   $(lang_label).text('Español');
@@ -48,7 +48,7 @@ $('.dropdown-content a:nth-of-type(2)').click(function(){
   loadFormbutton();
 })
 $('.dropdown-content a:nth-of-type(3)').click(function(){
-  lang = 'fr';
+  // lang = 'fr';
   $('.center-text a').attr("href","privacy.html?fr");
   window.history.replaceState(null, null, "?fr");
   $(lang_label).text('Français');
@@ -60,7 +60,7 @@ $('.dropdown-content a:nth-of-type(3)').click(function(){
   loadFormbutton();
 })
 $('.dropdown-content a:nth-of-type(4)').click(function(){
-  lang = 'de';
+  // lang = 'de';
   $('.center-text a').attr("href","privacy.html?de");
   window.history.replaceState(null, null, "?de");
   $(lang_label).text('Deutsche');
